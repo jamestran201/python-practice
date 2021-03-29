0373fa6e3a7f8f0dbd4d6ce79a198949d63ad253
James Tran <tmnhat2001@gmail.com>
2021-03-29 10:21:54 -0400

    Add safety package

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,7 @@
 import nox
+import tempfile
 
-nox.options.sessions = "lint", "tests"
+nox.options.sessions = "lint", "safety", "tests"
 
 
 @nox.session(python=["3.9", "3.8"])
@@ -31,3 +32,19 @@ def black(session):
     args = session.posargs or locations
     session.install("black")
     session.run("black", *args)
+
+
+@nox.session(python="3.9")
+def safety(session):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install("safety")
+        session.run("safety", "check", f"--file={requirements.name}", "--full-report")
