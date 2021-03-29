2d8c93423e90c27f4cc6411c439e710e29c5da1f
James Tran <tmnhat2001@gmail.com>
2021-03-29 09:48:19 -0400

    Add black to auto-format code

--- noxfile.py
+++ noxfile.py
@@ -1,15 +1,27 @@
 import nox
 
+nox.options.sessions = "lint", "tests"
+
+
 @nox.session(python=["3.9", "3.8"])
 def tests(session):
     args = session.posargs or ["--cov", "-m", "not e2e"]
     session.run("poetry", "install", external=True)
     session.run("pytest", *args)
 
+
 locations = "src", "tests", "noxfile.py"
+
 
 @nox.session(python=["3.9", "3.8"])
 def lint(session):
     args = session.posargs or locations
-    session.install("flake8")
+    session.install("flake8", "flake8-black")
     session.run("flake8", *args)
+
+
+@nox.session(python=["3.9", "3.8"])
+def black(session):
+    args = session.posargs or locations
+    session.install("black")
+    session.run("black", *args)
