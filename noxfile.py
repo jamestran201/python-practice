208d21ac52a18a5d068a3d0e654faf2699c238a7
James Tran <tmnhat2001@gmail.com>
2021-03-30 16:32:36 -0400

    Add static type checking with mypy

--- noxfile.py
+++ noxfile.py
@@ -2,7 +2,7 @@ import tempfile
 
 import nox
 
-nox.options.sessions = "lint", "safety", "tests"
+nox.options.sessions = "lint", "safety", "tests", "mypy"
 
 
 def install_with_constraints(session, *args, **kwargs):
@@ -67,3 +67,10 @@ def safety(session):
         )
         install_with_constraints(session, "safety")
         session.run("safety", "check", f"--file={requirements.name}", "--full-report")
+
+
+@nox.session(python=["3.9", "3.8"])
+def mypy(session):
+    args = session.posargs or locations
+    install_with_constraints(session, "mypy")
+    session.run("mypy", *args)
