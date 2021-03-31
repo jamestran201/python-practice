7a9969d9e489ea5e158b63ae74b855c5b8378624
James Tran <tmnhat2001@gmail.com>
2021-03-30 20:40:14 -0400

    Add pydantic to encapsulate JSON objects

--- src/hypermodern_python/console.py
+++ src/hypermodern_python/console.py
@@ -9,13 +9,10 @@ from . import wikipedia
 @click.command()
 @click.option("--language", "-l", "input_locale", default="")
 @click.version_option(version=__version__)
-def main(input_locale):
+def main(input_locale: str) -> None:
     """The hypermodern Python project."""
 
-    data = wikipedia.random_page(input_locale)
+    page = wikipedia.random_page(input_locale)
 
-    title = data["title"]
-    extract = data["extract"]
-
-    click.secho(title, fg="green")
-    click.echo(textwrap.fill(extract))
+    click.secho(page.title, fg="green")
+    click.echo(textwrap.fill(page.extract))
--- src/hypermodern_python/wikipedia.py
+++ src/hypermodern_python/wikipedia.py
@@ -2,28 +2,34 @@ import locale
 
 import click
 import requests
+from pydantic import BaseModel
 
 API_PATH = "https://{language}.wikipedia.org/api/rest_v1/page/random/summary"
 
 
-def random_page(language):
+class Page(BaseModel):
+    title: str
+    extract: str
+
+
+def random_page(language: str) -> Page:
     url = create_url(language)
 
     try:
         with requests.get(url) as response:
             response.raise_for_status()
-            return response.json()
-    except requests.exceptions.RequestException as error:
+            return Page(**response.json())
+    except (requests.exceptions.RequestException, TypeError) as error:
         message = str(error)
         raise click.ClickException(message)
 
 
-def create_url(input_locale):
+def create_url(input_locale: str) -> str:
     user_locale = input_locale or get_default_locale()
     return API_PATH.format(language=user_locale)
 
 
-def get_default_locale():
+def get_default_locale() -> str:
     default_locale, _ = locale.getdefaultlocale()
     if not default_locale:
         return "en"
